dee4fdfb78b9cd6f369176d06f511a164b0710b1
Yuri Artemev <i@artemeff.com>
2016-03-16 03:24:13 +0300

    fix migration ddl

--- lib/rethinkdb_ecto/migration.ex
+++ lib/rethinkdb_ecto/migration.ex
@@ -9,32 +9,32 @@ defmodule RethinkDB.Ecto.Migration do
       def supports_ddl_transaction?, do: false
 
       def execute_ddl(repo, {:create_if_not_exists, %Ecto.Migration.Table{name: name}, _fields}, _opts) do
-        ReQL.db(repo.config[:database]) |> ReQL.table_create(name) |> repo.run
+        ReQL.db(repo.config[:db]) |> ReQL.table_create(name) |> repo.run
         :ok
       end
 
       def execute_ddl(repo, {:create, %Ecto.Migration.Table{name: name}, _fields}, _opts) do
-        ReQL.db(repo.config[:database]) |> ReQL.table_create(name) |> repo.run
+        ReQL.db(repo.config[:db]) |> ReQL.table_create(name) |> repo.run
         :ok
       end
 
       def execute_ddl(repo, {:drop, %Ecto.Migration.Table{name: name}}, _opts) do
-        ReQL.db(repo.config[:database]) |> ReQL.table_drop(name) |> repo.run
+        ReQL.db(repo.config[:db]) |> ReQL.table_drop(name) |> repo.run
         :ok
       end
 
       def execute_ddl(repo, {:create, %Ecto.Migration.Index{columns: [column], table: table}}, _opts) do
-        ReQL.db(repo.config[:database]) |> ReQL.table(table) |> ReQL.index_create(column) |> repo.run
+        ReQL.db(repo.config[:db]) |> ReQL.table(table) |> ReQL.index_create(column) |> repo.run
         :ok
       end
 
       def execute_ddl(repo, {:drop_if_exists, %Ecto.Migration.Index{columns: [column], table: table}}, _opts) do
-        ReQL.db(repo.config[:database]) |> ReQL.table(table) |> ReQL.index_drop(column) |> repo.run
+        ReQL.db(repo.config[:db]) |> ReQL.table(table) |> ReQL.index_drop(column) |> repo.run
         :ok
       end
 
       def execute_ddl(repo, {:drop, %Ecto.Migration.Index{columns: [column], table: table}}, _opts) do
-        ReQL.db(repo.config[:database]) |> ReQL.table(table) |> ReQL.index_drop(column) |> repo.run
+        ReQL.db(repo.config[:db]) |> ReQL.table(table) |> ReQL.index_drop(column) |> repo.run
         :ok
       end
     end
